8c356bc2b9b9b82a952b19df9b49064b8df01d38
AnghelloVera132 <143229189+AnghelloVera132@users.noreply.github.com>
2023-11-28 22:47:16 -0500

    Update Program.cs

--- Laboratorio 13/Program.cs
+++ Laboratorio 13/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace Laboratorio_13
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Encuesta encuestas = new Encuesta();
+
+            do
+            {
+                Console.Clear();
+                Console.Write("===============================\n" +
+                "Encuestas de Calidad\n" +
+                "===============================\n" +
+                "1: Realizar Encuesta\n2: Ver datos registrados\n3: Eliminar un dato\n4: Ordenar datos de menor a mayor\n5: Salir\n" +
+                "===============================\n" +
+                "Ingrese una opción: ");
+                int option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        encuestas.quiz();
+                        break;
+                    case 2:
+                        encuestas.Data();
+                        break;
+                    case 3:
+                        encuestas.Eliminacion();
+                        break;
+                    case 4:
+                        encuestas.Ordenar();
+                        break;
+                    case 5:
+                        break;
+                }
+            } while (option != 5);
         }
     }
 }
